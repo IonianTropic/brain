fdcdf836311ca82c1fddc34170425efd173220dc
Sam Quatrone <samquatrone@gmail.com>
2022-12-16 15:53:55 -0700

    implemented create_layer Added a few comments/ideas Input nodes?

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::thread::current;
+
 const DEFAULT_WEIGHT: f64 = 1.0;
 
 fn main() {
@@ -16,16 +18,28 @@ fn create_network(nodes_per_layer: Vec<usize>) -> Vec<&'static Node> {
     // input case
     let mut prev_layer = ???
     // middle layer cases
-    for node_count in nodes_per_layer {
+    for node_count in nodes_per_layer { // Needs to iterate starting at nodes_per_layer[1] right?
         prev_layer = create_layer(prev_layer, node_count);
     }
     // output case
+    tail/return prev_layer
      */
 }
 
 fn create_layer(prev_layer: Vec<&'static Node>, node_count: usize) -> Vec<&'static Node> {
-    let current_layer = Vec::with_capacity(node_count);
-    
+    let current_layer: Vec<&'static Node> = Vec::with_capacity(node_count);
+    for i in 0..node_count {
+        current_layer[i] = &Node::from_inputs(prev_layer)
+    }
+    current_layer
+}
+
+fn create_input_layer(node_count: usize) -> Vec<&'static Node> {
+    let current_layer: Vec<&'static Node> = Vec::with_capacity(node_count);
+    for i in 0..node_count {
+        current_layer[i] = &Node::new()
+    }
+    current_layer
 }
 
 struct Node { // Neuron
@@ -38,6 +52,12 @@ impl Node {
         Self {
             inputs: Vec::new(),
             weights: Vec::new(),
+        }
+    }
+    fn new_input_node() -> Self {
+        Self {
+            // inputs: somehow just a value instead of Vec<Node>?
+            //weights: just 1? idk my brain fried
         }
     }
     fn from_inputs(inputs: Vec<&'static Node>) -> Self {
@@ -56,6 +76,11 @@ impl Node {
     }
     fn evaluate(&self) {
         // relu(sum(weights*inputs.evaluate()))
+        // FIXME: evaluate has to be different on input nodes
+        let sum: f64;
+        for i in 0..self.inputs.len() { //FIXME: make sure this looks good
+
+        }
     }
 }
 
