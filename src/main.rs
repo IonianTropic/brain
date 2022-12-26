13906d0db7df03620b6f3263654fcbb864045645
Sam Quatrone <samquatrone@gmail.com>
2022-12-26 13:20:52 -0700

    finished Network::new()

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,30 @@ mod math;
 const DEFAULT_WEIGHT: f64 = 1.0;
 const DEFAULT_VALUE: f64 = 0.0;
 
+//TODO: evaluate() function
+//TODO: implement accessible Edges for evaluate()
+
+//TODO: implement modify inputs/get outputs function to get the values of stored indices
 fn main() {
-    let mut node_list: Vec<f64> = Vec::new();
-    let mut edge_list: Vec<Edge> = Vec::new();
-    // network template: <2,3,1>
+    let template = vec![2,3,1];
 
-    // make input node(s) (0,1)
-    node_list.push(DEFAULT_VALUE);
-    node_list.push(DEFAULT_VALUE);
-    // make middle layer node(s) (2,3,4)
-    node_list.push(DEFAULT_VALUE);
-    node_list.push(DEFAULT_VALUE);
-    node_list.push(DEFAULT_VALUE);
-    //make output node(s) (5)
-    node_list.push(DEFAULT_VALUE);
+    let network = Network::new(template);
 
-    // connect inputs to middle
-    edge_list.push(Edge::new(0, 2));
-    edge_list.push(Edge::new(0, 3));
-    edge_list.push(Edge::new(0, 4));
-    edge_list.push(Edge::new(1, 2));
-    edge_list.push(Edge::new(1, 3));
-    edge_list.push(Edge::new(1, 4));
-    // connect middle to middle
-    // connect middle to output
-    edge_list.push(Edge::new(2, 5));
-    edge_list.push(Edge::new(3, 5));
-    edge_list.push(Edge::new(4, 5));
+    println!("input indices: ");
+    vprint(&network.inputs);
+
+    println!("output indices: ");
+    vprint(&network.outputs);
+
+    println!("node values: ");
+    vprint(&network.nodes);
+}
+
+fn vprint<T: std::fmt::Display> (vec: &Vec<T>) {
+    for i in vec {
+        print!("{} ", i);
+    }
+    println!();
 }
 
 struct Network {
@@ -48,25 +45,49 @@ impl Network {
             nodes.push(DEFAULT_VALUE);
         }
         // inputs
-        let inputs = Vec::new();
+        let mut inputs = Vec::new();
         for idx in 0..template[0] {
             inputs.push(idx);
         }
         // outputs
-        let outputs = Vec::new();
+        let mut outputs = Vec::new();
         for idx in node_count - template[template.len()-1]..node_count {
             outputs.push(idx);
         }
+
+        // layers
+        // EX with template <2,3,1>
+        // <<0,1>,<2,3,4>,<5>>
+        let mut layer_map: Vec<Vec<usize>> = Vec::new();
+        let mut node_idx: usize = 0;
+
+        for layer_nodes in template[0..].into_iter() {
+            let mut current_layer = Vec::new();
+            for _ in 0..*layer_nodes {
+                current_layer.push(node_idx);
+                node_idx += 1;
+            }
+            layer_map.push(current_layer);
+        }
+
         // edging
         let mut edges: Vec<Edge> = Vec::new();
-        // for (prev, current) in template[0..template.len()-1].into_iter().zip(template[1..template.len()].into_iter()) {
-        //     for i in 
-        // }
 
+        let prev_nodes_iter = layer_map[0..template.len()-1].into_iter(); //zero cost abstraction right?
+        let current_nodes_iter = layer_map[1..template.len()].into_iter();
+
+        // iterates over each adjacent pair of node layers and then the product of those two layers
+        for (prev, current) in prev_nodes_iter.zip(current_nodes_iter) {
+            for (i, j) in itertools::iproduct!(prev, current) {
+                edges.push(Edge::new(*i, *j)); // Is it fine to dereference here?
+            }
+        }
 
         Self {
             inputs,
             outputs,
+            nodes,
+            edges,
         }
     }
 }
